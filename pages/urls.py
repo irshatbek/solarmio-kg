8b59ebaad8507485a458d6912c04cef8d2e206c8
irshatbek <irshatbek@gmail.com>
2022-10-11 00:07:14 +0600

    added html files and static files

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('blank', views.blank, name='blank'),
+    path('buttons', views.buttons, name='buttons'),
+    path('flot', views.flot, name='flot'),
+    path('forms', views.forms, name='forms'),
+    path('grid', views.grid, name='grid'),
+    path('login', views.login, name='login'),
+    path('login', views.login, name='login'),
+    path('morris', views.morris, name='morris'),
+    path('motifications', views.motifications, name='motifications'),
+    path('panels_wells', views.panels_wells, name='panels_wells'),
+    path('tables', views.tables, name='tables'),
+    path('typography', views.typography, name='typography'),
+
 ]
+    
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,40 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def blank(request):
+    return render(request, 'pages/blank.html')
+
+def buttons(request):
+    return render(request, 'pages/buttons.html')
+
+def flot(request):
+    return render(request, 'pages/flot.html')
+
+def forms(request):
+    return render(request, 'pages/forms.html')
+
+def grid(request):
+    return render(request, 'pages/grid.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def morris(request):
+    return render(request, 'pages/morris.html')
+
+def motifications(request):
+    return render(request, 'pages/motifications.html')
+
+def panels_wells(request):
+    return render(request, 'pages/panels-wells.html')
+
+def tables(request):
+    return render(request, 'pages/tables.html')
+
+def typography(request):
+    return render(request, 'pages/typography.html')
+
